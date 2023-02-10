4ffcb778104d3bb2fa3248a8edf94c19c85a83aa
AnupKamble <sahyadrihillsagro@gmail.com>
2023-02-10 23:28:14 +0530

    userModel schema created

---
+++ TODO/server/models/user.models.js
@@ -0,0 +1,26 @@
+
+const mongoose = require('mongoose');
+
+const string = {'type':"String",required : true}
+
+const userSchema = new mongoose.Schema (
+
+    {
+        name : string,
+        gender : string,
+        email : string,
+        password : string,
+
+    },
+    {
+        timestamps: true,
+        versionKey:false
+
+    }
+)
+
+const userModel = mongoose.model( 'users' , userSchema);
+
+module.exports = {
+    userModel
+}
