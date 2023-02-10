8a434808e4d2957d1badd634c3df95259cf6c718
AnupKamble <sahyadrihillsagro@gmail.com>
2023-02-11 01:17:34 +0530

    log in successful by jwt

--- TODO/server/routes/auth.routes.js
+++ TODO/server/routes/auth.routes.js
@@ -4,43 +4,80 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const { Router } = require('express');
-const {userModel} = require('../models/user.models')
+const { userModel } = require('../models/user.models')
 
 
 const authRoutes = Router();
 
-authRoutes.get('/' , async(req,res)=> {
+authRoutes.get('/', async (req, res) => {
 
     res.send("wip");
 })
 
 
 
-authRoutes.post('/signup', async(req,res)=> {
-      
-     
-    const {name,gender,email,password} = req.body;
-      
-       const userPresent = await userModel.findOne({email});
-       if ( userPresent) {
-        res.send({'msg':'Already Exist! please try log in'});
-       }
+authRoutes.post('/signup', async (req, res) => {
+
+
+    const { name, gender, email, password } = req.body;
+
+    const userPresent = await userModel.findOne({ email });
+    if (userPresent) {
+        res.send({ 'msg': 'Already Exist! please try log in' });
+    }
     try {
-           
-    bcrypt.hash(password, 7 , async function (err,hash) {
 
-        const data = new userModel({name,gender,email,password:hash});
-        await data.save();
-        res.send('sign up successfully')
-    })
+        bcrypt.hash(password, 7, async function (err, hash) {
+
+            const data = new userModel({ name, gender, email, password: hash });
+            await data.save();
+            res.send('sign up successfully')
+        })
 
     }
-    catch(err) {
-        
-        res.status(500).send({'msg':'Something went wrong! please try again'})
+    catch (err) {
+
+        res.status(500).send({ 'msg': 'Something went wrong! please try again' })
     }
-   
+
 })
+
+
+authRoutes.post('/login', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    try {
+        const user = await userModel.find({ email })
+        
+        if (user.length > 0) {
+
+            const hashed_password = user[0].password;
+
+            bcrypt.compare(password, hashed_password, function (err, result) {
+
+                if (result) {
+
+                    const token = jwt.sign({ "userID": user[0]._id }, "secret");
+
+                    res.send({ "msg": "log in successful", "token": token});
+                }
+                else {
+                    res.send({ "msg": "log in failed" })
+                }
+            })
+        } else {
+            res.send({ "msg": "log in failed" })
+        }
+
+    }
+    catch (err) {
+       res.send({ "msg": "log in failed" })
+    }
+})
+
+
+
 
 
 module.exports = {
