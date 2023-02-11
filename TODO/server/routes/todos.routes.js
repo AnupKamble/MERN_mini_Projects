1250455f0bbbe65cbde757cb415e8e1ab8f9a8b4
AnupKamble <sahyadrihillsagro@gmail.com>
2023-02-11 12:43:06 +0530

    post and get routes done

--- TODO/server/routes/todos.routes.js
+++ TODO/server/routes/todos.routes.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const { Router } = require('express');
 const { Authenticate } = require('../middleware/Authenticate');
+const { todoModel } = require('../models/todos.models');
 
 
 const todosRoute = Router();
@@ -10,7 +11,53 @@ const todosRoute = Router();
 
 todosRoute.get('/', Authenticate, async (req, res) => {
 
-    res.send("i love cricket");
+    try {
+        const data = await todoModel.find();
+        res.send(data);
+    }
+    catch(err) {
+        res.send({"msg":"unexpected error"});
+    }
+})
+
+
+
+todosRoute.post('/create', Authenticate, async (req, res) => {
+    
+    const payload = req.body;
+
+    try {
+
+        const data = new todoModel(payload)
+              await data.save();
+            res.send({"msg": "successfully created post"}) 
+    }
+    catch(err) {
+        res.send({"msg":"unexpected error"});
+    }
+})
+
+
+
+todosRoute.patch('/update/:patchID', Authenticate, async (req, res) => {
+
+    try {
+        res.send("i love cricket");
+    }
+    catch(err) {
+        res.send({"msg":"unexpected error"});
+    }
+})
+
+
+todosRoute.delete('/delete/:deleteID', Authenticate, async (req, res) => {
+
+    try {
+        res.send("i love cricket");
+    }
+    catch(err) {
+        res.send({"msg":"unexpected error"});
+    }
 })
 
 
