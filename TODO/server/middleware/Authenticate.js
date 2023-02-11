c807b2d4f985ce806da52df3551dd989292f1c82
AnupKamble <sahyadrihillsagro@gmail.com>
2023-02-11 12:25:04 +0530

    authorization done

--- TODO/server/middleware/Authenticate.js
+++ TODO/server/middleware/Authenticate.js
@@ -1,20 +1,30 @@
 
 const jwt = require('jsonwebtoken');
 
-const Authenticate  =(req,res,next)=> {
+const Authenticate = (req, res, next) => {
 
-   
+
     const token = req.headers?.authorization?.split(" ")[1];
-    console.log(token);
+    // console.log(token);
 
-      if ( token) {
+    if (token) {
 
-        const decoded = jwt.verify( token , 'secret');
-          next();
-      }
-      else {
-        res.send({"msg" : "please login"})
-      }
+        const decoded = jwt.verify(token, 'secret');
+
+        if (decoded) {
+            // console.log(decoded);
+            const userID = decoded.userID;
+            req.body.userID = userID;
+            next();
+        }
+        else {
+            res.send({ "msg": "please login" })
+        }
+
+    }
+    else {
+        res.send({ "msg": "please login" })
+    }
 }
 
 
