a3974b2e5b872bf329aed97402482fc3f053b26c
AnupKamble <sahyadrihillsagro@gmail.com>
2023-02-11 17:11:35 +0530

    log in and signup done

--- TODO/client/src/Routes/AllRoutes.jsx
+++ TODO/client/src/Routes/AllRoutes.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Login from '../components/Login'
+// import Navbar from '../components/Navbar'
+import Signup from '../components/Signup'
 
 export default function AllRoutes() {
   return (
+
+
     <div>
+             
+             <Signup/>
+             <Login/>
       
     </div>
   )
--- TODO/client/src/components/Navbar.jsx
+++ TODO/client/src/components/Navbar.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { makeStyles } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
 
 export default function Navbar() {
+    const classes = useStyles();
   return (
-    <div>
-      
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Navbar
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }
